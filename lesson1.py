c01fd4edf0fa15e41e88f2ae744996a7346c9395
matthewmbagley <57724962+matthewmbagley@users.noreply.github.com>
2019-11-18 12:46:36 -0800

    Create lesson1.py
    This is for Christine's class

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+
+# Variable being assigned a string value
+subject = "Hallmarklabs' Lunch and Learn!"
+
+# Ask the user for their name
+user_name = input("What is your name? ")
+
+# Putting together string and a variable value together
+welcome_message = "Welcome " + user_name + ' to ' + subject
+
+# Display message to screen
+print(welcome_message)
